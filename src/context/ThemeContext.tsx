3ff817312f3473ca9ce20efef5b83b3f6b02b10c
Marcos Gomes <mpcgomes2@gmail.com>
2024-11-18 01:34:30 -0300

    add theme context remove theme redux

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,30 @@
+import React, { createContext, useEffect, useState } from 'react';
+
+interface ThemeContextProps {
+  theme: 'light' | 'dark';
+  toggleTheme: () => void;
+}
+
+export const ThemeContext = createContext<ThemeContextProps>({
+  theme: 'light',
+  toggleTheme: () => { },
+});
+
+export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [theme, setTheme] = useState<'light' | 'dark'>('light');
+
+  useEffect(() => {
+    const storedTheme = (localStorage.getItem('theme') as 'light' | 'dark') || 'light';
+    setTheme(storedTheme);
+    document.documentElement.setAttribute('data-theme', storedTheme);
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    document.documentElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+  };
+
+  return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>;
+};
