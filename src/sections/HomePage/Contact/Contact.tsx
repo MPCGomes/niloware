c6d78d5ea2be5cc4dfc575e1db51bf92029e2dce
Marcos Gomes <mpcgomes2@gmail.com>
2024-11-22 02:06:39 -0300

    add emailjs

--- src/sections/HomePage/Contact/Contact.tsx
+++ src/sections/HomePage/Contact/Contact.tsx
@@ -1,34 +1,25 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import ReCAPTCHA from 'react-google-recaptcha';
-import { useParams } from 'next/navigation';
-import { useTranslation } from '../../../hooks/useTranslation';
 import Button from '@/src/components/Button/Button';
 import styles from './Contact.module.scss';
 
 const Contact: React.FC = () => {
-  const { contact } = useTranslation();
-
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     phone: '',
-    message: ''
+    message: '',
   });
   const [captchaToken, setCaptchaToken] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const [success, setSuccess] = useState<boolean>(false);
 
-  const siteKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY;
-  const params = useParams();
-  const locale = params?.locale || 'en-us';
-
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
   };
 
@@ -43,50 +34,62 @@ const Contact: React.FC = () => {
     setSuccess(false);
 
     if (formData.name.length < 3) {
-      setError(contact?.errors.name || 'Name must be at least 3 characters.');
+      setError('Name must be at least 3 characters.');
       setLoading(false);
       return;
     }
     if (!/^\S+@\S+\.\S+$/.test(formData.email)) {
-      setError(contact?.errors.email || 'Please enter a valid email address.');
+      setError('Please enter a valid email address.');
       setLoading(false);
       return;
     }
     if (!/^\d{10,}$/.test(formData.phone)) {
-      setError(contact?.errors.phone || 'Phone number must be at least 10 digits.');
+      setError('Phone number must be at least 10 digits.');
       setLoading(false);
       return;
     }
     if (formData.message.length < 10) {
-      setError(contact?.errors.message || 'Message must be at least 10 characters.');
+      setError('Message must be at least 10 characters.');
       setLoading(false);
       return;
     }
     if (!captchaToken) {
-      setError(contact?.errors.captcha || 'Please complete the CAPTCHA.');
+      setError('Please complete the CAPTCHA.');
       setLoading(false);
       return;
     }
 
     try {
-      const response = await axios.post(`/api/contact`, {
-        ...formData,
-        captchaToken,
-        locale,
+      const response = await fetch('https://api.emailjs.com/api/v1.0/email/send', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          service_id: 'service_yyvleds',
+          template_id: 'template_w8h6p1f',
+          user_id: 'TV0egaxYnT67PB2Ez',
+          template_params: {
+            name: formData.name,
+            email: formData.email,
+            phone: formData.phone,
+            message: formData.message,
+          },
+        }),
       });
 
-      if (response.status === 200) {
+      if (response.ok) {
         setSuccess(true);
         setFormData({
           name: '',
           email: '',
           phone: '',
-          message: ''
+          message: '',
         });
         setCaptchaToken(null);
+      } else {
+        throw new Error('Failed to send the message. Please try again later.');
       }
     } catch (err) {
-      setError(contact?.errors.general || 'Failed to send message. Please try again later.');
+      setError('An error occurred. Please try again later.');
     } finally {
       setLoading(false);
     }
@@ -95,16 +98,12 @@ const Contact: React.FC = () => {
   return (
     <section className={styles.contact}>
       <div className={styles.container}>
-        <h2>
-          {contact?.title}
-        </h2>
-        <form
-          className={styles.inputs}
-          onSubmit={handleSubmit}>
+        <h2>Contact Us</h2>
+        <form className={styles.inputs} onSubmit={handleSubmit}>
           <input
             type="text"
             name="name"
-            placeholder={contact?.placeholders.name}
+            placeholder="Your Name"
             value={formData.name}
             onChange={handleChange}
             minLength={3}
@@ -114,7 +113,7 @@ const Contact: React.FC = () => {
             <input
               type="email"
               name="email"
-              placeholder={contact?.placeholders.email}
+              placeholder="Your Email"
               value={formData.email}
               onChange={handleChange}
               pattern="^\S+@\S+\.\S+$"
@@ -123,7 +122,7 @@ const Contact: React.FC = () => {
             <input
               type="text"
               name="phone"
-              placeholder={contact?.placeholders.phone}
+              placeholder="Your Phone"
               value={formData.phone}
               onChange={handleChange}
               pattern="^\d{10,}$"
@@ -132,7 +131,7 @@ const Contact: React.FC = () => {
           </div>
           <textarea
             name="message"
-            placeholder={contact?.placeholders.message}
+            placeholder="Your Message"
             value={formData.message}
             onChange={handleChange}
             minLength={10}
@@ -140,20 +139,17 @@ const Contact: React.FC = () => {
           />
           <ReCAPTCHA
             className={styles.captcha}
-            sitekey={siteKey || ''}
+            sitekey="6Lc9I1cqAAAAAH6ojKJq8mclozs2RaBgVgG4220F"
             onChange={handleCaptchaChange}
           />
           <Button
-            text={loading ? contact?.messages.sending : contact?.messages.send}
-            type="submit" className={styles.button}
+            text={loading ? 'Sending...' : 'Send Message'}
+            type="submit"
+            className={styles.button}
           />
         </form>
-        {error && <p className={styles.error}>
-          {error}
-        </p>}
-        {success && <p className={styles.success}>
-          {contact?.messages.success}
-        </p>}
+        {error && <p className={styles.error}>{error}</p>}
+        {success && <p className={styles.success}>Your message was sent successfully!</p>}
       </div>
     </section>
   );
