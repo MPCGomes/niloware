7f00b4d7a2b8868f7bb2c737b2b3a22317d550c1
Marcos Gomes <mpcgomes2@gmail.com>
2024-11-28 15:07:32 -0300

    update services

--- src/sections/HomePage/Services/Services.tsx
+++ src/sections/HomePage/Services/Services.tsx
@@ -4,41 +4,53 @@ import ServiceCard from '../../../components/ServiceCard/ServiceCard';
 import { useTranslation } from '../../../hooks/useTranslation';
 import { IoCheckmarkCircle } from 'react-icons/io5';
 
+interface Plan {
+    plan: string;
+    description: string;
+    textPrice: string;
+    price: string;
+    siteItems: string[];
+    buttonText: string;
+}
+
+interface ServicesTranslations {
+    title: string;
+    plans: Plan[];
+}
+
 const Services: React.FC = () => {
-    const { services } = useTranslation();
+    const { title, plans } = useTranslation<ServicesTranslations>('homePage', 'services');
+
     return (
         <section className={styles.services}>
             <div className={styles.container}>
                 <div className={styles.text}>
-                    <h2>
-                        {services?.title}
-                    </h2>
+                    <h2>{title}</h2>
                 </div>
                 <div className={styles.card}>
-                    {services?.plans?.map((plan, index) => (
+                    {plans?.map((plan, index) => (
                         <ServiceCard
                             key={index}
                             plan={plan.plan}
                             description={plan.description}
                             textPrice={plan.textPrice}
                             price={plan.price}
-                            siteItem={(
-                                <ul>
-                                    {plan.siteItems.map((item, i) => (
-                                        <li key={i}>
-                                            <span><IoCheckmarkCircle /></span> {item}
-                                        </li>
-                                    ))}
-                                </ul>
-                            )}
-                            buttonText={plan.buttonText}
-                            extraPrice={plan.extraPrice}
-                        />
+                            siteItem={<ul>
+                                {plan.siteItems.map((item, i) => (
+                                    <li key={i}>
+                                        <span>
+                                            <IoCheckmarkCircle />
+                                        </span>{' '}
+                                        {item}
+                                    </li>
+                                ))}
+                            </ul>}
+                            buttonText={plan.buttonText} extraPrice={''} />
                     ))}
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default Services
+export default Services;
