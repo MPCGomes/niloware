ed47dfb64cb8c0298b82fdf7fd3829466d7b7c55
Marcos Gomes <mpcgomes2@gmail.com>
2024-11-28 14:41:36 -0300

    update hero

--- src/sections/HomePage/Hero/Hero.tsx
+++ src/sections/HomePage/Hero/Hero.tsx
@@ -1,27 +1,27 @@
-import React from 'react'
+// Hero.tsx
+import React from 'react';
 import Button from '@/src/components/Button/Button';
 import { useTranslation } from '@/src/hooks/useTranslation';
-import styles from './Hero.module.scss'
+import styles from './Hero.module.scss';
+
+interface HeroTranslations {
+  title: string;
+  subtitle: string;
+  cta: string;
+}
 
 const Hero: React.FC = () => {
-  const { hero } = useTranslation();
+  const { title, subtitle, cta } = useTranslation<HeroTranslations>('homePage', 'hero');
 
   return (
     <header className={styles.hero}>
       <div className={styles.container}>
-        <h1>
-          {hero?.title}
-        </h1>
-        <h3>
-          {hero?.subtitle}
-        </h3>
-        <Button
-          link='#'
-          text={hero?.cta}
-        />
+        <h1>{title}</h1>
+        <h3>{subtitle}</h3>
+        <Button link="#" text={cta} />
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
