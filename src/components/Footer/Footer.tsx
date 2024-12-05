73a2f312e3f5be80c1694cabcd7da8c93524a6f0
Marcos Gomes <mpcgomes2@gmail.com>
2024-12-05 14:14:46 -0300

    update footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import { FaFacebook, FaInstagram } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
-import styles from './Footer.module.scss'
-import { useParams } from 'next/navigation';
+import React from 'react';
+import { FaInstagram } from 'react-icons/fa';
+import styles from './Footer.module.scss';
 import Link from 'next/link';
 import { useTranslation } from '../../../src/hooks/useTranslation';
 
+interface FooterTranslations {
+    footer: {
+        copyright: string;
+    };
+}
+
 const Footer = () => {
-    const params = useParams();
-    const locale = params.locale;
-    const { footer } = useTranslation();
+    const { t } = useTranslation<FooterTranslations>('common', 'footer');
+
+    if (!t || !t.footer) {
+        return null;
+    }
 
     return (
         <footer className={styles.footer}>
@@ -23,41 +29,20 @@ const Footer = () => {
                             className={styles.social}
                             href=""
                         >
-                            <FaFacebook 
-                            />
-                        </Link>
-                        <Link
-                            className={styles.social}
-                            href=""
-                        >
                             <FaInstagram />
-                        </Link>
-                        <Link
-                            className={styles.social}
-                            href=""
-                        >
-                            <FaXTwitter />
                         </Link>
                     </div>
                 </div>
                 <div className={styles.rightContainer}>
-                    <div className={styles.links}>
-                        <Link href={`/${locale}/tos`}>
-                            {footer?.tos}
-                        </Link>
-                        <Link href={`/${locale}/privacy`}>
-                            {footer?.privacy}
-                        </Link>
-                    </div>
                     <div className={styles.copyright}>
                         <p>
-                            {footer?.copyright}
+                            {t.footer.copyright}
                         </p>
                     </div>
                 </div>
             </div>
         </footer>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
