644d0899b0632a7badcbe52a82be988f4e282a80
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-02-14 14:14:04 -0300

    feat: button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Button.module.scss'
+
+const Button = () => {
+  return (
+    <button className={styles.btn}>
+        Placeholder
+    </button>
+  )
+}
+
+export default Button
