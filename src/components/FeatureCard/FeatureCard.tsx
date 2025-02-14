591eb56d503b6b46dc14ea1a1ceaab1f1911941f
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-02-14 15:25:04 -0300

    feat: feature card component

---
+++ src/components/FeatureCard/FeatureCard.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./FeatureCard.module.scss";
+
+interface FeatureCardProps {
+  icon: React.ReactNode;
+  title: React.ReactNode;
+  description: React.ReactNode;
+  iconColor: string;
+}
+
+const FeatureCard: React.FC<FeatureCardProps> = ({
+  icon,
+  title,
+  description,
+  iconColor,
+}) => {
+  return (
+    <div className={styles.featureCard}>
+      <p 
+      className={styles.icon} 
+      style={{ color: iconColor }}
+      >
+        {icon}
+      </p>
+      <p className={styles.title}>{title}</p>
+      <p className={styles.description}>{description}</p>
+    </div>
+  );
+};
+
+export default FeatureCard;
