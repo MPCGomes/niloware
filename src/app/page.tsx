89c06ba1e1ba3eed35868fb7caaadf574c440263
Marcos Gomes <mpcgomes2@gmail.com>
2025-02-14 15:15:47 -0300

    add testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import TestimonialsSection from "@/containers/homePage/testimonialsSection/TestimonialsSection";
 import styles from "./page.module.scss";
 
 export default function Home() {
-  return <div className={styles.page}></div>;
+  return (
+    <div className={styles.page}>
+      <TestimonialsSection />
+    </div>
+  );
 }
