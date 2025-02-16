e4c5fd825f9137b1bc8ab41aa4bad581e5f99dee
Marcos Gomes <mpcgomes2@gmail.com>
2025-02-16 18:06:14 -0300

    fix hero section

--- src/containers/homePage/heroSection/HeroSection.tsx
+++ src/containers/homePage/heroSection/HeroSection.tsx
@@ -5,13 +5,13 @@ import styles from "./HeroSection.module.scss";
 const HeroSection = () => {
   return (
     <section className={styles.hero}>
-        <div className="container section">
-          <h1>Conquiste mais clientes com um site profissional</h1>
-          <h3>
-            Um negócio confiável consegue mais vendas. Criamos sites que fazem
-            as pessoas escolherem você.
-          </h3>
-          <Button text="Peça um Orçamento" />
+      <div className="container section">
+        <h1>Conquiste mais clientes com um site profissional</h1>
+        <h3>
+          Um negócio confiável consegue mais vendas. Criamos sites que fazem as
+          pessoas escolherem você.
+        </h3>
+        <Button text="Peça um Orçamento" />
       </div>
     </section>
   );
