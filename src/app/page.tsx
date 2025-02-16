24c382f0d4f5f6afee25d3ea80a56b1cdc52b9cd
Marcos Gomes <mpcgomes2@gmail.com>
2025-02-15 22:44:13 -0300

    update page with ready sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import TestimonialsSection from "@/containers/homePage/testimonialsSection/TestimonialsSection";
 import styles from "./page.module.scss";
 import PricingToggle from "@/components/PricingToggle/PricingToggle";
+import HeroSection from "@/containers/homePage/heroSection/HeroSection";
+import FeaturesSection from "@/containers/homePage/featuresSection/FeaturesSection";
+import PortfolioSection from "@/containers/homePage/portfolioSection/PortfolioSection";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <HeroSection />
+      <FeaturesSection />
+      <PortfolioSection />
       <PricingToggle />
       <TestimonialsSection />
     </div>
