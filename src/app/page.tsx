f3571356daad8519e72e2d466aa03e968694e70b
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-02-17 10:52:12 -0300

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import HeroSection from "@/containers/homePage/heroSection/HeroSection";
 import FeaturesSection from "@/containers/homePage/featuresSection/FeaturesSection";
 import PortfolioSection from "@/containers/homePage/portfolioSection/PortfolioSection";
 import ClientsSection from "@/containers/homePage/clientsSection/ClientsSection";
+import PricingSection from "@/containers/homePage/pricingSection/PricingSection";
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
       <ClientsSection />
       <FeaturesSection />
       <PortfolioSection />
-      <PricingToggle />
+      <PricingSection />
       <TestimonialsSection />
     </div>
   );
