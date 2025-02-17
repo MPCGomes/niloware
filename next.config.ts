6a809089a7258f69bb95a9dbf6cfaade7c692045
Marcos Gomes <mpcgomes2@gmail.com>
2025-02-17 14:37:29 -0300

    add export settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
