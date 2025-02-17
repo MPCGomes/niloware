53e9f91a5c12415412145259d94da8664356fd0f
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-02-17 10:25:07 -0300

    refactor: props update

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,12 +4,24 @@ import styles from "./Button.module.scss";
 interface ButtonProps {
   text: React.ReactNode;
   icon?: React.ReactNode;
+  color: string;
+  borderColor: string;
   onClick?: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, icon, onClick }) => {
+const Button: React.FC<ButtonProps> = ({
+  text,
+  icon,
+  color,
+  borderColor,
+  onClick,
+}) => {
   return (
-    <button onClick={onClick} className={styles.button}>
+    <button 
+    style={{color: color, border: `1px solid ${borderColor}`}}
+    onClick={onClick} 
+    className={styles.button}
+    >
       {icon && <span className={styles.icon}>{icon}</span>}
       {text}
     </button>
