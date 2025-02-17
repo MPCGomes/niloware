c3f5aabe7beaad49b52be879e962cd75e3bc314f
Marcos Gomes <mpcgomes2@gmail.com>
2025-02-17 14:33:59 -0300

    fix inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" className={inter.className}>
       <body>
         <Header />
         {children}
