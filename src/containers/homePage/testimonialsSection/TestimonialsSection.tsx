dc3455c0d7c2833429b8eb97087780e09a322f85
Marcos Gomes <mpcgomes2@gmail.com>
2025-02-28 18:24:04 -0300

    refactor testimonials section

--- src/containers/homePage/testimonialsSection/TestimonialsSection.tsx
+++ src/containers/homePage/testimonialsSection/TestimonialsSection.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { FC, useEffect, useState } from "react";
-import axios from "axios";
-import TestimonialsCarousel from "../../../components/TestimonialsCarousel/TestimonialsCarousel";
+import TestimonialsCarousel from "@/components/TestimonialsCarousel/TestimonialsCarousel";
 import SectionHeading from "@/components/SectionHeading/SectionHeading";
 
 interface Testimonial {
@@ -14,27 +13,29 @@ interface Testimonial {
 }
 
 const TestimonialsSection: FC = () => {
-  const [testimonials, setTestimonials] = useState<Testimonial[]>([]);
+  const [testimonials, setTestimonials] = useState<Testimonial[] | null>(null);
 
   useEffect(() => {
     const fetchTestimonials = async () => {
       try {
-        const response = await axios.get("/content/testimonials.json");
-        setTestimonials(response.data);
-      } catch (error) {
-        console.error("Failed to load testimonials:", error);
+        const response = await fetch("/content/testimonials.json");
+        const data = await response.json();
+        if (data.length) setTestimonials(data);
+      } catch {
+        console.error("Erro ao carregar depoimentos");
       }
     };
 
     fetchTestimonials();
   }, []);
 
+  if (!testimonials) return null;
+
   return (
     <section className="container section">
       <SectionHeading
         subheading="Confira o que nossos clientes estão dizendo"
         heading="Depoimentos"
-        link={""}
       />
       {testimonials.length > 0 && (
         <TestimonialsCarousel testimonials={testimonials} />
