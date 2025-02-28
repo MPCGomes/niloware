26843a62a9596d81c535dd7691407a2e1965ee3c
Marcos Gomes <mpcgomes2@gmail.com>
2025-02-28 16:59:57 -0300

    refactor header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,17 +7,22 @@ import { WhatsApp } from "@mui/icons-material";
 import styles from "./Header.module.scss";
 
 const Header: React.FC = () => {
-  const whatsappLink = `https://wa.me/5512974096393?text=Olá, estou interessado nos seus serviços!`;
+  const whatsappLink =
+    "https://wa.me/5512974096393?text=Olá, estou interessado nos seus serviços!";
+
+  const openWhatsApp = () => {
+    window.open(whatsappLink, "_blank");
+  };
 
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <div className={styles.section}>
-          <Logo size="md" light />
+          <Logo size="md" />
           <Button
             text={<span className={styles.buttonText}>+55 12 97409-6393</span>}
             icon={<WhatsApp fontSize="small" />}
-            onClick={() => window.open(whatsappLink, "_blank")}
+            onClick={openWhatsApp}
           />
         </div>
       </div>
