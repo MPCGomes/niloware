8af07930a2d408de0907c4df59cedb2cc454bfec
Marcos Gomes <mpcgomes2@gmail.com>
2025-02-28 13:08:32 -0300

    rename feature to tag in portfolio card

--- src/components/PortfolioCard/PortfolioCard.tsx
+++ src/components/PortfolioCard/PortfolioCard.tsx
@@ -3,14 +3,14 @@ import styles from "./PortfolioCard.module.scss";
 
 interface PortfolioCardProps {
   title: React.ReactNode;
-  features: React.ReactNode[];
+  tags: React.ReactNode[];
   imageBackground: string;
   isFirst: boolean;
 }
 
 const PortfolioCard: React.FC<PortfolioCardProps> = ({
   title,
-  features,
+  tags,
   imageBackground,
   isFirst,
 }) => {
@@ -19,14 +19,17 @@ const PortfolioCard: React.FC<PortfolioCardProps> = ({
       className={styles.portfolioCard}
       style={{
         backgroundImage: `url(${imageBackground})`,
+<<<<<<< Updated upstream
         height: isFirst ? "380px" : "230px",
+=======
+>>>>>>> Stashed changes
       }}
     >
       <p className={styles.title}>{title}</p>
-      <div className={styles.featureContainer}>
-        {features.map((feature, index) => (
-          <p key={index} className={styles.feature}>
-            {feature}
+      <div className={styles.tagContainer}>
+        {tags.map((tag, index) => (
+          <p key={index} className={styles.tag}>
+            {tag}
           </p>
         ))}
       </div>
