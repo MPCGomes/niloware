525a1048bbc31bcacbbac72b9efbbf6bd9f0c9e5
Marcos Gomes <mpcgomes2@gmail.com>
2025-02-28 18:25:40 -0300

    refactor cta section

--- src/containers/homePage/ctaSection/CtaSection.tsx
+++ src/containers/homePage/ctaSection/CtaSection.tsx
@@ -5,16 +5,19 @@ import React from "react";
 import styles from "./CtaSection.module.scss";
 
 const CtaSection = () => {
-  const whatsappLink = `https://wa.me/5512974096393?text=Olá, estou interessado nos seus serviços!`;
+  const whatsappLink =
+    "https://wa.me/5512997591515?text=Olá, estou interessado nos seus serviços!";
 
   return (
     <section className={styles.ctaSection}>
       <div className="container">
-        <p>Seu negócio merece crescer — Vamos fazer isso acontecer?</p>
-        <Button
-          text={<span className={styles.buttonText}>Peça um Orçamento</span>}
-          onClick={() => window.open(whatsappLink, "_blank")}
-        />
+        <div className={styles.content}>
+          <p>Seu negócio merece crescer — Vamos fazer isso acontecer?</p>
+          <Button
+            text={<span className={styles.buttonText}>Peça um Orçamento</span>}
+            onClick={() => window.open(whatsappLink, "_blank")}
+          />
+        </div>
       </div>
     </section>
   );
