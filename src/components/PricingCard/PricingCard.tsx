24e811b1eb8297926fbeeefec37c1e3691a4ff77
Marcos Gomes <mpcgomes2@gmail.com>
2025-02-28 17:00:15 -0300

    refactor pricing card

--- src/components/PricingCard/PricingCard.tsx
+++ src/components/PricingCard/PricingCard.tsx
@@ -1,62 +1,55 @@
 import React from "react";
+import clsx from "clsx";
 import styles from "./PricingCard.module.scss";
 import Button from "../Button/Button";
 import { Check as CheckIcon } from "@mui/icons-material";
 
 interface PricingCardProps {
   plan: string;
-  mostPopular: string;
+  mostPopular?: boolean;
   price: string;
-  monthlyPrice: string;
+  monthlyPrice?: string;
   features: string[];
-  backgroundGradient: string;
-  textColor: string;
-  btnColor: string;
-  btnBorder: string;
-  listColor: string;
-  checkColor: string;
+  variant?: "default" | "popular";
 }
 
 const PricingCard: React.FC<PricingCardProps> = ({
   plan,
-  mostPopular,
+  mostPopular = false,
   price,
   monthlyPrice,
   features,
-  backgroundGradient,
-  textColor,
-  btnColor,
-  btnBorder,
-  listColor,
-  checkColor,
+  variant = "default",
 }) => {
   const whatsappLink = `https://wa.me/5512974096393?text=Olá, quero contratar o plano ${encodeURIComponent(
     plan
   )}.`;
 
   return (
-    <div
-      style={{ background: backgroundGradient, color: textColor }}
-      className={styles.pricingCard}
-    >
+    <div className={clsx(styles.pricingCard, styles[variant])}>
       <div className={styles.plan}>
         <p>{plan}</p>
-        {mostPopular && <p className={styles.mostPopular}>{mostPopular}</p>}
+        {mostPopular && <p className={styles.mostPopular}>Mais Popular</p>}
       </div>
       <p className={styles.price}>{price}</p>
       {monthlyPrice && <p className={styles.monthlyPrice}>{monthlyPrice}</p>}
+
       <Button
         text="Contratar"
-        color={btnColor}
-        borderColor={btnBorder}
+        color={variant === "popular" ? "default" : "subtle"}
         onClick={() => window.open(whatsappLink, "_blank")}
       />
+
       <ul className={styles.featureList}>
         {features.map((feature, index) => (
-          <li key={index} style={{ color: listColor }}>
-            <span style={{ color: checkColor }}>
+          <li key={index} className={styles.listItem}>
+            <span
+              className={clsx(styles.checkIcon, {
+                [styles.popularIcon]: variant === "popular",
+              })}
+            >
               <CheckIcon fontSize="small" />
-            </span>{" "}
+            </span>
             {feature}
           </li>
         ))}
