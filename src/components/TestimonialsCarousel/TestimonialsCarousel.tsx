274484d4dcb299a2c5dadb390216b07d67d6ae90
Marcos Gomes <mpcgomes2@gmail.com>
2025-02-28 18:23:36 -0300

    fix testimonials mobile

--- src/components/TestimonialsCarousel/TestimonialsCarousel.tsx
+++ src/components/TestimonialsCarousel/TestimonialsCarousel.tsx
@@ -25,14 +25,14 @@ const TestimonialsCarousel: React.FC<TestimonialsCarouselProps> = ({
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 4,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 5000,
     cssEase: "linear",
     fade: false,
     centerMode: false,
-    variableWidth: false,
+    variableWidth: true,
     responsive: [
       {
         breakpoint: 1024,
