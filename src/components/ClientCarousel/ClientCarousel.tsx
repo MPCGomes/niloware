de94b19a98bb0e3fd0f5e861061bd4d005426ef7
Marcos Gomes <mpcgomes2@gmail.com>
2025-02-28 16:59:39 -0300

    refactor client carousel

--- src/components/ClientCarousel/ClientCarousel.tsx
+++ src/components/ClientCarousel/ClientCarousel.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import React from "react";
+import dynamic from "next/dynamic";
 import styles from "./ClientCarousel.module.scss";
 import Image from "next/image";
+
+const Slider = dynamic(() => import("react-slick"), { ssr: false });
 
 const clientLogos = [
   "/client-logos/logumrh.webp",
@@ -17,48 +17,40 @@ const clientLogos = [
   "/client-logos/pousada-lilabella.webp",
 ];
 
+const settings = {
+  dots: false,
+  arrows: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  cssEase: "linear",
+  fade: false,
+  centerMode: true,
+  variableWidth: true,
+};
+
 const ClientCarousel: React.FC = () => {
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  const settings = {
-    dots: false,
-    arrows: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    cssEase: "linear",
-    fade: false,
-    centerMode: true,
-    variableWidth: true,
-  };
-
   return (
-    <div className={`${styles.wrapper} ${isMounted ? styles.visible : ""}`}>
-      {isMounted && (
-        <Slider {...settings} className={styles.carousel}>
-          {clientLogos.map((src, index) => (
-            <div key={index} className={styles.imageWrapper}>
-              <Image
-                src={src}
-                alt={`Client ${index}`}
-                className={styles.image}
-                width={0}
-                height={28}
-                sizes="auto"
-                style={{ width: "auto", height: "28px" }}
-                draggable="false"
-              />
-            </div>
-          ))}
-        </Slider>
-      )}
+    <div className={styles.wrapper}>
+      <Slider {...settings} className={styles.carousel}>
+        {clientLogos.map((src) => (
+          <div key={src} className={styles.imageWrapper}>
+            <Image
+              src={src}
+              alt="Client Logo"
+              className={styles.image}
+              width={0}
+              height={28}
+              sizes="auto"
+              style={{ width: "auto", height: "28px" }}
+              draggable="false"
+            />
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
