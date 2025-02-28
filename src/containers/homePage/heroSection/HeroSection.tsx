dc7197cc1a4aa62cde237841b5cf2c7271f85648
Marcos Gomes <mpcgomes2@gmail.com>
2025-02-28 18:24:12 -0300

    refactor hero section

--- src/containers/homePage/heroSection/HeroSection.tsx
+++ src/containers/homePage/heroSection/HeroSection.tsx
@@ -6,16 +6,18 @@ import styles from "./HeroSection.module.scss";
 import ClientsSection from "../clientsSection/ClientsSection";
 
 const HeroSection = () => {
-  const whatsappLink = `https://wa.me/5512974096393?text=Olá, estou interessado nos seus serviços!`;
+  const whatsappLink = `https://wa.me/5512997591515?text=Olá, estou interessado nos seus serviços!`;
 
   return (
     <section className={styles.hero}>
       <div className="container section">
-        <h1>Conquiste mais clientes com um site profissional</h1>
-        <h3>
+        <h1 className={styles.heading}>
+          Conquiste mais clientes com um site profissional
+        </h1>
+        <h2 className={styles.subheading}>
           Um negócio confiável consegue mais vendas. Criamos sites que fazem as
           pessoas escolherem você.
-        </h3>
+        </h2>
         <Button
           text={<span className={styles.buttonText}>Peça um Orçamento</span>}
           onClick={() => window.open(whatsappLink, "_blank")}
