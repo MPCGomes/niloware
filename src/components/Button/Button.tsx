66f7d0e395dc0438d776cfc34e3c753f76d3e888
Marcos Gomes <mpcgomes2@gmail.com>
2025-03-01 01:13:47 -0300

    update button variant

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,17 +5,17 @@ import styles from "./Button.module.scss";
 interface ButtonProps {
   text: React.ReactNode;
   icon?: React.ReactNode;
-  color?: "default" | "subtle";
+  variant?: "default" | "subtle";
   onClick?: () => void;
 }
 
 const Button: FC<ButtonProps> = ({
   text,
   icon,
-  color = "default",
+  variant = "default",
   onClick,
 }) => (
-  <button className={clsx(styles.button, styles[color])} onClick={onClick}>
+  <button className={clsx(styles.button, styles[variant])} onClick={onClick}>
     {icon && <span className={styles.icon}>{icon}</span>}
     {text}
   </button>
