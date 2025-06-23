c5b03845f1e75ed8726969eead6e100dd200c829
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-06-23 14:25:32 -0300

    style (button): hover animation

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -20,11 +20,11 @@ const Button: FC<ButtonProps> = ({
       className={clsx(
         "px-[24px] py-[10px] rounded-[24px] border-2 cursor-pointer flex items-center justify-center gap-[8px] font-medium bg-transparent",
         variant === "default" &&
-          "border-[var(--color-text-white)] text-[var(--color-text-white)]",
+          "border-[var(--color-text-white)] text-[var(--color-text-white)] hover:bg-[var(--color-text-white)] hover:text-[var(--color-primary)] transition-all duration-300 ease-in-out",
         variant === "subtle" &&
           "border-gray-300 text-[var(--color-text-primary)]",
         variant === "full" &&
-          "bg-white border-white text-[var(--color-primary)]"
+          "bg-white border-white text-[var(--color-primary)] hover:bg-gray-100 hover:border-gray-100 transition-all duration-300 ease-in-out"
       )}
     >
       {icon && <span className="flex items-center">{icon}</span>}
