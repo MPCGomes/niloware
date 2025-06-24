9a2b1113f70af2a340ab6c8e5094ee6be623a32f
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-06-24 14:48:26 -0300

    style (footer): new design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,42 +5,66 @@ import Logo from "./Logo";
 import { Instagram, WhatsApp } from "@mui/icons-material";
 import { useTranslations } from "next-intl";
 
-const Footer: FC = () => {
+interface FooterProps {
+  locale: string;
+}
+
+const Footer: FC<FooterProps> = ({ locale }) => {
   const t = useTranslations("common.footer");
 
   return (
     <footer className="bg-[#111111] text-[var(--color-text-secondary)]">
-      <div className="container">
-        <div className="flex flex-col items-center text-center gap-[16px] px-[16px] py-[50px] lg:flex-row lg:justify-between lg:text-left lg:gap-0 lg:px-[120px]">
-          <div className="flex flex-col items-center lg:items-start">
-            <Logo size="xxl" variant="dark" />
-            <div className="flex gap-[8px]">
-              <a
-                href="https://instagram.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Instagram
-                  fontSize="medium"
-                  className="text-[var(--color-text-tertiary)]"
-                />
-              </a>
-              <a
-                href="https://wa.me/0123456789012"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <WhatsApp
-                  fontSize="medium"
-                  className="text-[var(--color-text-tertiary)]"
-                />
-              </a>
+      <div className="container flex flex-col items-center text-center gap-[24px] px-5 py-[48px] lg:flex-col lg:items-start">
+        {/* Top side */}
+        <div className="flex flex-col items-center gap-8 lg:w-full lg:flex-row lg:justify-between lg:items-start">
+          <Logo size="xxl" variant="dark" />
+          {/* Right Side */}
+          <div className="flex flex-col gap-12 lg:flex-row">
+            {/* Quick Links */}
+            <div className="text-[var(--color-text-tertiary)] flex flex-col gap-[18px] ">
+              <p className="text-base text-center font-bold lg:text-left ">Quick Links</p>
+              <ul className="flex flex-col gap-3 ">
+                <li className="lg:text-left">
+                  <a href={`/${locale}`} className="underline">
+                    Home
+                  </a>
+                </li>
+                <li className="lg:text-left">
+                  <a href={`/${locale}/portfolio`} className="underline">
+                    Portfolio
+                  </a>
+                </li>
+              </ul>
+            </div>
+            {/* Social Media */}
+            <div className="text-[var(--color-text-tertiary)] flex flex-col gap-[18px]">
+              <p className="text-base text-center font-bold">Social Media</p>
+              {/* Icons */}
+              <div className="flex gap-3 justify-center lg:justify-start">
+                <a
+                  href="https://instagram.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Instagram fontSize="medium" />
+                </a>
+                <a
+                  href="https://wa.me/0123456789012"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <WhatsApp fontSize="medium" />
+                </a>
+              </div>
             </div>
           </div>
-          <p className="text-center lg:text-left text-[var(--color-text-tertiary)]">
-            {t("copyright")}
-          </p>
         </div>
+        {/* Horizontal Line */}
+        <hr className="border border-t-0 border-[var(--color-text-tertiary)] w-full" />
+        {/* Copyright */}
+        <p className="text-center lg:text-left text-[var(--color-text-tertiary)]">
+          {t("copyright")}
+        </p>
       </div>
     </footer>
   );
