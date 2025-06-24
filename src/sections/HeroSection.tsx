c21a0852149420a93a28be896a83f6e31920862c
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-06-24 13:29:19 -0300

    style (hero): new background

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -2,7 +2,6 @@
 
 import type { FC } from "react";
 import Button from "@/components/Button";
-import Image from "next/image";
 import { useTranslations } from "next-intl";
 import { useWhatsappLink } from "@/hooks/useWhatsappLink";
 
@@ -11,44 +10,28 @@ const HeroSection: FC = () => {
   const whatsappLink = useWhatsappLink("homepage.hero", "whatsappText");
 
   return (
-    <section className="bg-primary-gradient">
-      <div className="container flex flex-col items-center px-[20px] md:px-[16px] py-20 lg:py-30 lg:flex-row-reverse lg:items-center lg:gap-20">
-        {/* Left Side (Image) */}
-        <div className="lg:flex-1">
-          <div className="relative aspect-[1/1] w-[200px] md:w-[300px] lg:w-[550px]">
-            <Image
-              src="/hero-image.svg"
-              alt="hero-image"
-              fill
-              className="object-contain"
-              priority
-            />
-          </div>
+    <section className="bg-primary-gradient bg-hero h-screen">
+      <div className="container flex flex-col gap-16 items-center justify-center h-full px-[20px] md:px-[16px] py-20 lg:py-30 lg:items-start ">
+        {/* Text */}
+        <div className="flex flex-col gap-[18px] text-[var(--color-text-primary)] text-center lg:text-left">
+          <h1 className="text-[2rem] desktop:text-[2.875rem] font-extrabold lg:w-[800px]">
+            {t("heading")}
+          </h1>
+          <h2 className="mobile:text-base tablet:text-[1.125rem] lg:w-[600px]">
+            {t("subheading")}
+          </h2>
         </div>
-
-        {/* Right Side (Text + Buttons) */}
-        <div className="flex flex-col gap-[56px] items-center text-center lg:items-start lg:text-left lg:flex-1">
-          {/* Text */}
-          <div className="flex flex-col gap-[18px]">
-            <h1 className="text-[2rem] desktop:text-[2.875rem] font-extrabold text-white">
-              {t("heading")}
-            </h1>
-            <h2 className="text-[var(--color-text-white)] mobile:text-base tablet:text-[1.125rem]">
-              {t("subheading")}
-            </h2>
-          </div>
-          {/* Buttons */}
-          <div className="flex gap-5">
-            <Button
-              variant="full"
-              text={<span className="text-base">{t("cta")}</span>}
-              onClick={() => window.open(whatsappLink, "_blank")}
-            />
-            <Button
-              text={<span className="text-base">{t("cta")}</span>}
-              onClick={() => window.open(whatsappLink, "_blank")}
-            />
-          </div>
+        {/* Buttons */}
+        <div className="flex gap-5">
+          <Button
+            variant="full"
+            text={<span className="text-base">{t("cta")}</span>}
+            onClick={() => window.open(whatsappLink, "_blank")}
+          />
+          <Button
+            text={<span className="text-base">{t("cta")}</span>}
+            onClick={() => window.open(whatsappLink, "_blank")}
+          />
         </div>
       </div>
     </section>
