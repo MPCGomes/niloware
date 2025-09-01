6f237c232169fe0580ca2df13753dbd18b396133
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-09-01 18:16:12 -0300

    style (cta): text size

--- src/components/Cta/CtaSection.tsx
+++ src/components/Cta/CtaSection.tsx
@@ -11,7 +11,7 @@ const CtaSection: FC<CtaSectionProps> = ({}) => {
     <section className="bg-[#000130] p-[30px]">
       <div className="container mx-auto">
         <div className="flex flex-col items-center text-center gap-8 text-[var(--color-text-white)] text-base md:flex-row md:text-left md:justify-center lg:gap-16">
-          <p>Your business deserves to grow—ready to make it happen?</p>
+          <p className="text-2xl">Your business deserves to grow—ready to make it happen?</p>
           Button
         </div>
       </div>
